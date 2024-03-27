7d03d0915ba3ef06eccf593c7951b7efb54e0d08
Ramsay Leung <ramsayleung@gmail.com>
2024-03-26 18:53:21 -0700

    Initial Commit

---
+++ Freshzilla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Freshzilla
+//
+//  Created by ramsayleung on 2024-03-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Freshzilla/FreshzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshzillaApp.swift
+//  Freshzilla
+//
+//  Created by ramsayleung on 2024-03-26.
+//
+
+import SwiftUI
+
+@main
+struct FreshzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
